1836e9122fe4d5a397362376ac2d67ed2f32ff85
Sulaiman <s444444466@gmail.com>
2025-03-10 12:02:30 +0700

    Update mahasiswa.php

--- mahasiswa.php
+++ mahasiswa.php
@@ -9,10 +9,7 @@
     <h6> Data Mahasiswa AMP <h/6>
     <br>
     <h6>NIM : 23110007</h6><br>
-    <h6>Nama : Sulaiman</h6><br>
-    <h6>Ketua Kelas</h6><br>
-    <h6>NIM : 23110007</h6><br>
-    <h6>asep</h6>    
+    <h6>Nama : Sulaiman</h6><br>  
     <a href="index.php">Kembali</a>
 </body>
 </html>
