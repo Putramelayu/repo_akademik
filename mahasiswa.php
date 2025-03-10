66731aac14b68920e840fae5974af486e0f14519
Sulaiman <s444444466@gmail.com>
2025-03-10 11:47:08 +0700

    Create mahasiswa.php
    Buat modul mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h6> Data Mahasiswa AMP <h/6>
+    <br>
+    <h6>NIM : 23110007</h6><br>
+    <h6>Nama : Sulaiman</h6><br>
+    <a href="index.php">Kembali</a>
+</body>
+</html>
