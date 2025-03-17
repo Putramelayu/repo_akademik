843a16523bd07b12d7846b48a63d963e37b13e57
Sulaiman <s444444466@gmail.com>
2025-03-17 10:47:18 +0700

    Update mahasiswa.php
    perubahan mahasiswa

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Mahasiswa</title>
+<title>Data Mahasiswa</title>
 </head>
 <body>
-    <h6> Data Mahasiswa AMP <h/6>
-    <br>
-    <h6>NIM : 23110007</h6><br>
-    <h6>Nama : Sulaiman</h6><br>  
-    <a href="index.php">Kembali</a>
+<h6>Data Mahasiswa AMP</h6>
+<hr>
+<h6>NIM : 111111</h6><br>
+<h6>Nama : Khosyi Ariqdzakwan</h6><br>
+<h6>Ketua Kelas</h6><br>
+<h6>NIM : 23110025</h6><br>
+<h6>Sulaiman</h6><br>
+<h6>Tahun 2025</h6>
+<a href="index.php">Kembali</a>
 </body>
 </html>
